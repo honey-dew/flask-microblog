81f7d20b706c9904f844ed52e4d94415f8759d76
honeydew <adiwiguna.git@gmail.com>
2020-02-16 14:51:27 +0700

    part4

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 from secrets import token_hex
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 
 	SECRET_KEY = token_hex(24)
+
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')or \
+	'sqlite:///' + os.path.join(basedir, 'app.db')
+
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
